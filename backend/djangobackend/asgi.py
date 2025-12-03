2fa82b059dbc745ef0390da412b701a7aa88af71
Noela Andosay <144686117+NMAndosay@users.noreply.github.com>
2025-12-03 20:04:29 +0800

    fixed backend setup issues

--- backend/djangobackend/asgi.py
+++ backend/djangobackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for geobackend project.
+ASGI config for djangobackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geobackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobackend.settings')
 
 application = get_asgi_application()
--- backend/djangobackend/wsgi.py
+++ backend/djangobackend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for geobackend project.
+WSGI config for djangobackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geobackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobackend.settings')
 
 application = get_wsgi_application()

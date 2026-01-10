f805a96c187d0c71368d73621e3f776f599b55ce
VinceArbutante <vincejosepharbutante@gmail.com>
2026-01-11 01:31:04 +0800

    added reverse geocoding for real time location setup in ReportDrawer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.auth_views import get_current_user, sign_up, login_user
 from .views.views import MyTokenObtainPairView, RegisterView, testEndPoint, testEndPoint, getRoutes
+from .views.geocoding import reverse_geocode
 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
@@ -16,4 +17,5 @@ urlpatterns = [
     path("current_user/", get_current_user, name="get_current_user"),
     path('sign_up/', sign_up, name='sign_up'),
     path("login_user/", login_user, name="login_user"),
+    path("geocoding/reverse/", reverse_geocode, name="reverse_geocode"),
 ]

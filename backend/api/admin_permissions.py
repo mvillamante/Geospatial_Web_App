b4dc6aa42835d08bd87911a0a62f20064a322603
VinceArbutante <vincejosepharbutante@gmail.com>
2026-01-22 21:15:17 +0800

    removed backend

--- backend/api/admin_permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminRole(BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user
-            and request.user.is_authenticated
-            and request.user.role.lower() == "admin"
-        )

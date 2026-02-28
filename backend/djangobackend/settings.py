534584712fc96840ff7664a7f86ecaf47fc1c726
Michaella Villamante <villamantemichaella5@gmail.com>
2026-02-28 18:42:10 +0800

    Fix DATABASES configuration

--- backend/djangobackend/settings.py
+++ backend/djangobackend/settings.py
@@ -112,11 +112,9 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(
-            default=os.environ.get("DATABASE_URL")
-        )
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # Password validation

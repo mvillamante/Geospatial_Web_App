5fd10e7eaa9af3e074d7b3b71eb1c4ec87525fdf
Michaella Villamante <villamantemichaella5@gmail.com>
2026-03-08 23:14:52 +0800

    Add PWA icons

--- frontend/geospatial-app-vite/vite.config.ts
+++ frontend/geospatial-app-vite/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       workbox: {
         maximumFileSizeToCacheInBytes: 5000000
       },
-      
+
       devOptions: {
         enabled: true
       },
@@ -30,12 +30,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/pwa/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/pwa/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }

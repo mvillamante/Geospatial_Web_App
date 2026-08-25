2a32211bfb07130c3d583b6a5b3dcc0368fdd55c
Vince Joseph Arbutante <VinceArbu>
2026-08-25 15:07:53 +0800

    improve comments in backend for clarity

--- frontend/geospatial-app-vite/src/libr/supabaseClient.ts
+++ frontend/geospatial-app-vite/src/libr/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-//for supabase
+// Initialize Supabase backend client configuration
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or key is missing in environment variables");
 } 
